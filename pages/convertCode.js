2c44f977f72edb416cf63defecd80b85f9d05f3f
Ryuke <sahalrajiv-extc@atharvacoe.ac.in>
2023-04-23 16:25:40 +0530

    Add nav component

--- pages/convertCode.js
+++ pages/convertCode.js
@@ -1,10 +1,12 @@
 import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 function ConvertCode() {
   return (
     <main>
+      <Navbar />
       <div>
         <h1
           className={`${inter.className} text-red-500 m-5 text-4xl font-bold`}
