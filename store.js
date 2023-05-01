7e6ebe86c9e8d7d959c92e97781100da447f5624
Ryuke <sahalrajiv-extc@atharvacoe.ac.in>
2023-05-01 19:56:02 +0530

    Create zustand store containing all state values

---
+++ store.js
@@ -0,0 +1,27 @@
+import { create } from "zustand";
+
+// creating a store function that takes in a set argument
+// this set argument is essentially a setter function that returns an object
+// that returned object will contain all the values that we've defined in our store
+const store = (set) => ({
+  codeToBeConverted: "",
+  codeToBeExplained: "",
+  convertedCode: "",
+
+  // onchange handlers for the above code values
+  setCodeToBeConverted: (newValue) => {
+    set(() => ({
+      codeToBeConverted: newValue,
+    }));
+  },
+  setCodeToBeExplained: (newValue) =>
+    set(() => ({
+      codeToBeExplained: newValue,
+    })),
+  setConvertedCode: (newValue) =>
+    set(() => ({
+      convertedCode: newValue,
+    })),
+});
+
+export const useStore = create(store);
