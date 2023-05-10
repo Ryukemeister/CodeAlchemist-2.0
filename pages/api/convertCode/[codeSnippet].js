0e918f4391c1835f648a5cf538912bee295c4bac
Ryuke <sahalrajiv-extc@atharvacoe.ac.in>
2023-05-10 21:33:11 +0530

    Splitting incoming request in the api and modify user content

--- pages/api/convertCode/[codeSnippet].js
+++ pages/api/convertCode/[codeSnippet].js
@@ -3,6 +3,15 @@ import { Configuration, OpenAIApi } from "openai";
 export default async function handler(req, res) {
   if (req.method === "GET") {
     const { codeSnippet } = req.query;
+
+    // Splitting incoming codeSnippet to get currentLang
+    // langToConvert and actual code
+    const splitCodeSnippet = codeSnippet.split("=+x=");
+    const currentLanguage = splitCodeSnippet[0];
+    const languageToConvert = splitCodeSnippet[1];
+    const actualCode = splitCodeSnippet[2];
+
+    // Getting api key
     const openaiApiKey = process.env.OPENAI_API_KEY;
 
     const configuration = new Configuration({
@@ -12,19 +21,18 @@ export default async function handler(req, res) {
     // setting up a new configration for the opneai api
     const openai = new OpenAIApi(configuration);
 
-    // calling the api and assigning the system the desired role
+    // Calling the api and assigning the system the desired role
     // in order to get a response that can be as close to our expectation
     const response = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
       messages: [
         {
           role: "system",
-          content:
-            "You are a code converter that that translates code from one programming language into another with precision without giving any notes",
+          content: "You are a helpful assistant",
         },
         {
           role: "user",
-          content: `Translate the following piece of code from javascript to python without suggesting any notes: "{${codeSnippet}}"`,
+          content: `Translate the following piece of code from ${currentLanguage} to ${languageToConvert} without suggesting any notes: "${actualCode}"`,
         },
       ],
     });
