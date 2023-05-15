82cc430989a7c5a4ef26b550928a569a10573e3a
Ryuke <sahalrajiv-extc@atharvacoe.ac.in>
2023-05-15 06:01:31 +0530

    Modifying api model and adjusting prompts

--- pages/api/convertCode/[codeSnippet].js
+++ pages/api/convertCode/[codeSnippet].js
@@ -11,11 +11,8 @@ export default async function handler(req, res) {
     const languageToConvert = splitCodeSnippet[1];
     const actualCode = splitCodeSnippet[2];
 
-    // Getting api key
-    const openaiApiKey = process.env.OPENAI_API_KEY;
-
     const configuration = new Configuration({
-      apiKey: openaiApiKey,
+      apiKey: process.env.OPENAI_API_KEY,
     });
 
     // setting up a new configration for the opneai api
@@ -23,21 +20,41 @@ export default async function handler(req, res) {
 
     // Calling the api and assigning the system the desired role
     // in order to get a response that can be as close to our expectation
-    const response = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: "system",
-          content: "You are a helpful assistant",
-        },
-        {
-          role: "user",
-          content: `Translate the following piece of code from ${currentLanguage} to ${languageToConvert} without suggesting any notes: "${actualCode}"`,
-        },
-      ],
+    // const response = await openai.createChatCompletion({
+    //   model: "gpt-3.5-turbo",
+    //   messages: [
+    //     {
+    //       role: "system",
+    //       content: "You are a helpful assistant",
+    //     },
+    //     {
+    //       role: "user",
+    //       content: `Translate the following piece of code from ${currentLanguage} to ${languageToConvert} without suggesting any notes: "${actualCode}"`,
+    //     },
+    //   ],
+    // });
+
+    // console.log(actualCode, typeof actualCode);
+    // console.log(
+    //   currentLanguage,
+    //   typeof currentLanguage,
+    //   languageToConvert,
+    //   typeof languageToConvert
+    // );
+
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `##### Translate this piece of code from ${currentLanguage} into ${languageToConvert}\n### ${currentLanguage}\n ${actualCode} \n### ${languageToConvert}`,
+      temperature: 0,
+      max_tokens: 150,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+      stop: ["###"],
     });
 
-    const filteredReponse = response.data.choices[0].message;
+    // const filteredReponse = response.data.choices[0].message;
+    const filteredReponse = response.data;
 
     res.status(200).json({ filteredReponse });
   }
