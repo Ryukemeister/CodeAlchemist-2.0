32fe4a07ace3fc79410669eb7ee5544d6993c0bc
Ryuke <sahalrajiv-extc@atharvacoe.ac.in>
2023-05-15 05:44:02 +0530

    Add input for getting code

--- pages/explainCode.js
+++ pages/explainCode.js
@@ -1,9 +1,24 @@
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
+import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 function ExplainCode() {
+  const [inputVaue, setInputValue] = useState("");
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(`/api/explainCode/${inputVaue}`);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <main>
       <Navbar />
@@ -13,6 +28,19 @@ function ExplainCode() {
         >
           This is where all the code explanation happens
         </h1>
+        <input
+          className="border-2 border-red-500 px-2 mx-5"
+          value={inputVaue}
+          onChange={(e) => {
+            setInputValue(e.currentTarget.value);
+          }}
+        ></input>
+        <button
+          onClick={handleClick}
+          className="bg-yellow-500 px-2 py-1 rounded-sm text-white"
+        >
+          Click me
+        </button>
       </div>
     </main>
   );
