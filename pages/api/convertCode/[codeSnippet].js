b285f144437eecca463a742cc529876813bc567e
Ryuke <sahalrajiv-extc@atharvacoe.ac.in>
2023-05-15 06:30:54 +0530

    Refactoring reponse function again

--- pages/api/convertCode/[codeSnippet].js
+++ pages/api/convertCode/[codeSnippet].js
@@ -44,13 +44,13 @@ export default async function handler(req, res) {
 
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `Translate the given piece of code from ${currentLanguage} into ${languageToConvert}: ${actualCode}`,
+      prompt: `##### Translate this function from ${currentLanguage} into ${languageToConvert}\n### ${currentLanguage}\n ${actualCode} \n### ${languageToConvert}`,
       temperature: 0,
       max_tokens: 150,
-      // top_p: 1.0,
-      // frequency_penalty: 0.0,
-      // presence_penalty: 0.0,
-      // stop: ["###"],
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+      stop: ["###"],
     });
 
     // const filteredReponse = response.data.choices[0].message;
