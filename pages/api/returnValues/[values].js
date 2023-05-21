a08828f557d7e00025d2ccd36ba027e944acf0c8
Ryuke <sahalrajiv-extc@atharvacoe.ac.in>
2023-05-21 13:20:25 +0530

    Fix variable names

--- pages/api/returnValues/[values].js
+++ pages/api/returnValues/[values].js
@@ -4,9 +4,9 @@ export default function handler(req, res) {
   if (req.method === "GET") {
     const { values } = req.query;
     const splitCode = values.split(" ");
-    const currentLanguage = splitCode[0].slice(1, -1);
-    const languageToConvert = splitCode[1].slice(1, -1);
-    const actualCode = splitCode[2].slice(1, -1);
+    const currentLangg = splitCode[0].slice(1, -1);
+    const langToConvertt = splitCode[1].slice(1, -1);
+    const actualCodeToConvert = splitCode[2].slice(1, -1);
 
     res.status(200).json({
       values,
