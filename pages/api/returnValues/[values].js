5b112aae9bab7cdf06562c164cd7d0b81a1bfba7
Ryuke <sahalrajiv-extc@atharvacoe.ac.in>
2023-05-21 11:38:19 +0530

    Add endpoint for returning values

---
+++ pages/api/returnValues/[values].js
@@ -0,0 +1,11 @@
+export default function handler(req, res) {
+  if (req.method === "GET") {
+    const { values } = req.query;
+    const splitCode = values.split("+");
+    const x = splitCode[0];
+    const y = splitCode[1];
+    const z = splitCode[2];
+
+    res.status(200).json({ splitCode, x, y, z });
+  }
+}
