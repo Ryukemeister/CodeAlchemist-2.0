1121d1233bc9b036c620f0a96615ea4b7a236c4c
Ryuke <sahalrajiv-extc@atharvacoe.ac.in>
2023-05-21 12:47:04 +0530

    Returning actual value

--- pages/api/returnValues/[values].js
+++ pages/api/returnValues/[values].js
@@ -1,4 +1,6 @@
 export default function handler(req, res) {
+  // let x, y, z, codeSplit;
+
   if (req.method === "GET") {
     const { values } = req.query;
     const splitCode = values.split("+");
@@ -6,6 +8,6 @@ export default function handler(req, res) {
     const y = splitCode[1];
     const z = splitCode[2];
 
-    res.status(200).json({ splitCode, x, y, z });
+    res.status(200).json({ splitCode, x, y, z, values });
   }
 }
