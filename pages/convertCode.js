83ce2a6756d9645d9337dc8c6e0b18f001b54c5c
Ryuke <sahalrajiv-extc@atharvacoe.ac.in>
2023-05-28 15:43:08 +0530

    Cleanup and add support for flutter and C++

--- pages/convertCode.js
+++ pages/convertCode.js
@@ -1,9 +1,8 @@
 import { useStore } from "@/store";
-import { Inter, Poppins } from "next/font/google";
+import { Poppins } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import Editor from "@/components/Editor";
 
-const inter = Inter({ subsets: ["latin"] });
 const poppins = Poppins({ subsets: ["latin"], weight: ["400", "800"] });
 
 function ConvertCode() {
@@ -16,83 +15,15 @@ function ConvertCode() {
     "TypeScript",
     "Python",
     "Java",
-    "Natural language",
+    "C++",
     "C",
     "Sql",
     "Swift",
     "Ruby",
-    "Regex",
+    "Flutter",
     "Go",
     "Dart",
   ];
-
-  async function onSubmitButton(e) {
-    e.preventDefault();
-
-    // Obtaining values of the current language options and
-    // the language to be converted
-    const currentLang = document.getElementById("current-language").value;
-    const langToConvert = document.getElementById(
-      "language-to-be-converted"
-    ).value;
-    const errMessage = `// Oops, something's wrong :(
-
-const error = {
-  causes:[
-    "Either you haven't provided any input to the Editor",
-    "Or the language that you're trying to convert your code into and the language that you're currently opting for in the Editor are both one and the same",
-    "Or maybe you aren't connected to the internet"
-  ], 
-  possibleSolutions:[
-    "Try providing a better prompt to the Editor",
-    "Or check if the language that you write your code and the language you want to convert your code into are not the same",
-    "Or try refreshing your browser"
-  ],
-}; `;
-
-    // Error handling
-    // Try to check if the user provides a blank input to the Editor
-    // Or the language to convert and current language are the same
-    if (
-      codeToBeConverted.length <= 0 ||
-      currentLang == langToConvert ||
-      codeToBeConverted == errMessage
-    ) {
-      setConvertedCode(errMessage);
-      return;
-    }
-
-    setConvertedCode("Loading...");
-
-    const response = await fetch(
-      `/api/convertCode/(${currentLang})~(${langToConvert})~(${codeToBeConverted})`
-    );
-    const data = await response.json();
-
-    // const { responseFromBard } = data;
-    const { languageToConvert, currentLanguage, actualCode } = data;
-
-    console.log(languageToConvert, currentLanguage);
-    console.log(actualCode);
-
-    // Since we know the bard response goes something like
-    // "Sure, here is the translation
-    // ``` code ``` always comes after the above sentence
-    // split text using ```
-    // const splitResponeFromBard = responseFromBard.split("```");
-
-    // if (
-    //   splitResponeFromBard[1].startsWith(
-    //     langToConvert[0].toLocaleLowerCase() + langToConvert.slice(1)
-    //   )
-    // ) {
-    //   const splitCodeToDisplay = splitResponeFromBard[1].split("\n");
-    //   splitCodeToDisplay.shift();
-    //   setConvertedCode(splitCodeToDisplay.join("\n"));
-    // } else {
-    //   setConvertedCode(splitResponeFromBard[1].trimStart().trimEnd());
-    // }
-  }
 
   async function onButtonClick(e) {
     e.preventDefault();
@@ -185,15 +116,9 @@ const error = {
       </div>
       <button
         className={`${poppins.className} bg-red-500 opacity-90 text-white font-poppins font-semibold text-xl px-3 py-1 rounded-md tracking-wide ml-5 md:ml-10 mb-5 mt-5`}
-        onClick={onSubmitButton}
-      >
-        Submit
-      </button>
-      <button
-        className={`${poppins.className} bg-red-500 opacity-90 text-white font-poppins font-semibold text-xl px-3 py-1 rounded-md tracking-wide ml-5 md:ml-10 mb-5 mt-5`}
         onClick={onButtonClick}
       >
-        Click me
+        Submit
       </button>
     </main>
   );
