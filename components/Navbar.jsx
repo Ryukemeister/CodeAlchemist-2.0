ea7762ccbf2498d5082aa87010475328303af5d2
Ryuke <sahalrajiv-extc@atharvacoe.ac.in>
2023-06-04 12:54:59 +0530

    Add sidebar for small screeen devices

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,25 @@
 import Link from "next/link";
-import { Inter, Poppins } from "next/font/google";
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+import Sidebar from "./Sidebar";
 
-const inter = Inter({ subsets: ["latin"] });
 const poppins = Poppins({ subsets: ["latin"], weight: ["400", "800"] });
 
 function Navbar() {
+  function displaySidebar() {
+    const hamburgerMenu = document.querySelector(".hamburger-navigation");
+    hamburgerMenu.classList.remove("translate-x-[-100%]");
+  }
+
+  function hideSidebar() {
+    const hamburgerMenu = document.querySelector(".hamburger-navigation");
+    hamburgerMenu.classList.add("translate-x-[-100%]");
+  }
+
   return (
     <nav className="flex bg-black text-white h-[15vh] md:h-[12vh] items-center justify-between overflow-hidden">
       <div className="md:pl-10">
+        <Sidebar handleClick={hideSidebar} />
         <Link href="/">
           <h1
             className={`${poppins.className} pl-5 md:pl-0 bg-gradient-to-r from-[#8A2387] via-[#E94057] to-[#F27121] text-transparent bg-clip-text font-bold text-[28px]`}
@@ -15,6 +27,17 @@ function Navbar() {
             CodeAlchemist
           </h1>
         </Link>
+      </div>
+      <div className="block md:hidden">
+        <button onClick={displaySidebar}>
+          <Image
+            src="/Hamburger_Icon.jpg"
+            className="mr-5"
+            width="50"
+            height="40"
+            alt="Hamburger icon"
+          />
+        </button>
       </div>
       <div className="hidden md:block pr-10">
         <ul className="flex gap-x-14">
